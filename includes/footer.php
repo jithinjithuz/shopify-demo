189171b25b4b7a591b83ba93b49359e7703d4cb1
JITHIN RAJ P <jithinrajp505@gmail.com>
2025-03-25 22:06:32 +0530

    footer change

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,61 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+<style>
+    
+</style>
+<head>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+<div class="footer">
+            <div class="container">     
+                <div class="row">                       
+                    <div class="col-lg-4 col-sm-4 col-xs-12">
+                        <div class="single_footer">
+                            <h4>PARTNERS</h4>
+                            <ul>
+                                <li><a href="#">Lorem Ipsum</a></li>
+                                <li><a href="#">Simply dummy text</a></li>
+                                <li><a href="#">The printing and typesetting </a></li>
+                                <li><a href="#">Standard dummy text</a></li>
+                                <li><a href="#">Type specimen book</a></li>
+                            </ul>
+                        </div>
+                    </div><!--- END COL --> 
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+                        <div class="single_footer single_footer_address">
+                            <h4>Page Link</h4>
+                            <ul>
+                                <li><a href="#">Lorem Ipsum</a></li>
+                                <li><a href="#">Simply dummy text</a></li>
+                                <li><a href="#">The printing and typesetting </a></li>
+                                <li><a href="#">Standard dummy text</a></li>
+                                <li><a href="#">Type specimen book</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+                        <div class="single_footer single_footer_address">
+                            <h4>Subscribe today</h4>
+                            <div class="signup_form">                           
+                                <form action="#" class="subscribe">
+                                    <input type="text" class="subscribe__input" placeholder="Enter Email Address">
+                                    <button type="button" class="subscribe__btn"><i class="fas fa-paper-plane"></i></button>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="social_profile">
+                            <ul>
+                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                            </ul>
+                        </div>                          
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12 col-sm-12 col-xs-12">
+                        <p class="copyright">Copyright © 2025 <a href="#">shopify design</a>.</p>
+                    </div>          
+                </div>     
+            </div>
+        </div>
